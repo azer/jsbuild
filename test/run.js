a99898d15071cdc4aa8621e273346c6a8c859fd2
E.Azer Koçulu <azer@kodfabrik.com>
2011-09-13 02:33:58 -0700

    improve test runner

--- test/run.js
+++ test/run.js
@@ -4,26 +4,48 @@ var testModule = require('./tests'),
             .map(function(el){ return testModule[el] }),
     puts = require('sys').puts;
 
+var mark = function(){
+
+  var ctr = 0,
+      len = tests.length,
+      fail = 0;
+
+  return function(test,error){
+    ctr++;
+    error && fail++;
+
+    process.stdout.write('> '+test.name + ' ('+ctr+'/'+len+' , ' + ( (new Date).getTime() - startTS )/1000+'s) ');
+    puts(error ? 'FAIL ('+error.message+', '+error.stack+')' : '  OK');
+    testModule.reset();
+
+    if(ctr>=len){
+      puts('====');
+      puts('Ran '+len+' tests '  + ( fail ? 'with ' + fail + ' fail.' : 'without any error.') );
+    }
+  }
+
+}();
+
 var test, next, startTS;
 (function(i){
 
   if(i>=tests.length){
-    puts('====');
-    puts('Ran '+tests.length+' tests without any error.');
+    puts('* All tests has been fired.');
     return;
   }
 
   test = tests[i];
   next = arguments.callee.bind(undefined, i+1);
 
-  process.stdout.write('Running "'+test.name+'" ...');
+  puts('Running "'+test.name+'" ...');
 
   startTS = (new Date).getTime();
-  tests[i](function(error){
-    if(error) throw error;
-    puts('  OK ('+( (new Date).getTime() - startTS )/1000+'s) ');
-    testModule.reset();
-    next();
-  });
+  try {
+    tests[i](mark.bind(undefined,test));
+  } catch(error){
+    mark(test,error)
+  }
+
+  next();
 
 })(0);
