cc8988f1951e5c7e73effbbced0bee3753821964
E.Azer Koçulu <azer@kodfabrik.com>
2011-09-13 02:35:17 -0700

    tests

--- test/tests.js
+++ test/tests.js
@@ -23,68 +23,99 @@ function test_buildPackage(callback){
   });
 }
 
-function test_collectModules(callback){
-  jsbuild.collectModules('./test/example-project',function(error, files){
-    var expectedModules = ['lib/a.js',
-    'lib/b.js',
-    'node_modules/dependency/lib/f.js',
-    'node_modules/dependency/lib/g.js',
-    'node_modules/dependency/node_modules/subdependency/lib/i.js',
-    'node_modules/dependency/node_modules/subdependency/lib/j.js']
-    assert.equal(files.length, expectedModules.length);
-    for(var i = -1, len=expectedModules.length; ++i < len; ){
-      assert.equal(files[i],expectedModules[i]);
-    };
+function test_collectDeps(callback){
+  var pkg = { 
+    'name':'example-project',
+    'manifest':{ 
+      'dependencies':{ 
+        'dependency':'*',
+        'sibling':'*'
+      } 
+    },
+    'workingDir':'test/example-project/',
+    'packageDict':{}
+  };
 
+  jsbuild.collectDeps(pkg, function(error, deps){
+    assert.equal(deps.length, 2);
+    assert.equal(deps[0].name, 'dependency');
+    assert.equal(deps[0].dependencies[0].name, 'subdependency');
+    assert.equal(deps[1].name, 'sibling');
     callback();
+  });
+}
+
+function test_loadPackage(callback){
+  jsbuild.loadPackage('test/example-project/', function(error, pkg){
+    if(error){
+      throw error;
+    }
+    assert.equal(pkg.name, 'example-project');
+    assert.equal(pkg.manifest.name, 'example-project');
+    assert.equal(pkg.manifestPath, 'test/example-project/package.json');
+    assert.equal(pkg.dependencies.length, 2);
+
+    var pkgDict = Object.keys(pkg.packageDict);
+    assert.equal(pkgDict.length, 4);
+    assert.equal(pkgDict[0], 'example-project');
+    assert.equal(pkgDict[1], 'dependency');
+    assert.equal(pkgDict[2], 'subdependency');
+    assert.equal(pkgDict[3], 'sibling');
+
+    assert.equal(pkg.modules.length, 2);
+    assert.equal(pkg.modules[1].filename, 'lib/a.js');
+    assert.equal(pkg.modules[0].filename, 'lib/b.js');
+    callback();
+  });
+}
+
+function test_collectModules(callback){
+  jsbuild.collectModules({ 'workingDir':'test/example-project/' }, function(error, modules){
+    try {
+      assert.equal(modules.length, 2);
+      assert.equal(modules[1].filename, 'lib/a.js');
+      assert.equal(modules[0].filename, 'lib/b.js');
+      callback();
+    } catch(exc) {
+      callback(exc);
+    }
   });
 }
 
 function test_filterFilename(callback){
 
-  var legalPaths = ['lib/foo.js','lib/bar/qux.js','lib/qux/quux.js','node_modules/foo/lib/bar.js'],
-      illegalPaths = ['lib/foo','qux.js','lib/qux.j','node_modules/foo/test/qux.js'];
+  var legalPaths = ['foo.js','lib/bar/qux.js','lib/qux/quux.js','node_modules/foo/lib/bar.js'],
+      illegalPaths = ['lib/foo','lib/qux.j'];
 
-  var testLegals,testIllegals;
+  for(var i = -1, len=legalPaths.length; ++i < len; ){
+    assert.ok(jsbuild.filterFilename(legalPaths[i]));
+  };
 
-  var next;
+  for(var i = -1, len=illegalPaths.length; ++i < len; ){
+    assert.ok(!jsbuild.filterFilename(illegalPaths[i]));
+  };
 
-  (function(i){
-    if(i>=legalPaths.length){
-      testLegals = true;
-      if(testLegals && testIllegals){
-        callback();
-      }
-      return;
-    }
+  callback();
+}
 
-    next = arguments.callee.bind(undefined,i+1);
+function test_loadModule(callback){
+  jsbuild.loadModule('test/example-project/lib/a.js', function(error, module){
+    assert.equal(module.name, 'a');
+    assert.equal(module.filename, 'test/example-project/lib/a.js');
+    assert.equal(module.content, 'require(\'dependency\');\nconsole.log(\'a.js\');\n');
+    callback();
+  });
+}
 
-    jsbuild.filterFilename(legalPaths[i],function(result){
-      assert.ok(result);
-      next();
-    });
-
-  })(0);
-
-  (function(i){
-    if(i>=illegalPaths.length){
-      testIllegals = true;
-      if(testLegals && testIllegals){
-        callback();
-      }
-      return;
-    }
-
-    next = arguments.callee.bind(undefined,i+1);
-
-    jsbuild.filterFilename(illegalPaths[i],function(result){
-      assert.ok(!result);
-      next();
-    });
-
-  })(0);
-
+function test_moduleName(callback){
+  assert.equal(jsbuild.moduleName('foo.js'),'foo');
+  assert.equal(jsbuild.moduleName('foo/bar/qux.js'),'qux');
+  assert.equal(jsbuild.moduleName('foo'));
+  assert.equal(jsbuild.moduleName('foo/bar/qux'));
+  assert.equal(jsbuild.moduleName('foo.js/bar.js/qux'));
+  assert.equal(jsbuild.moduleName('foo.js/bar.js/qux.js.'));
+  assert.equal(jsbuild.moduleName('qux/quux/c-orge.js'),'c-orge');
+  callback();
 }
 
 function reset(){
@@ -96,5 +127,9 @@ module.exports = {
   'test_buildPackage':test_buildPackage,
   'test_collectModules':test_collectModules,
   'test_filterFilename':test_filterFilename,
+  'test_loadPackage':test_loadPackage,
+  'test_collectDeps':test_collectDeps,
+  'test_loadModule':test_loadModule,
+  'test_moduleName':test_moduleName,
   'reset':reset
 }
